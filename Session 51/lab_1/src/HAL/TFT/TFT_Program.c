3df82a0f41256a5757d19769d872e24f09ac58aa
Ahmed Abdalla <127306647+AhmedzAbdalla@users.noreply.github.com>
2023-06-06 17:06:42 +0300

    Add files via upload

---
+++ Session 51/lab_1/src/HAL/TFT/TFT_Program.c
@@ -0,0 +1,23 @@
+/*
+ * TFT_Program.c
+ *
+ *  Created on: June 02, 2023
+ *  Author       : Ahmed Abdalla
+ */
+
+
+/*Standard Libraries include*/
+#include  "../../include/LIB/STD_Types.h"
+#include  "../../include/LIB/BIT_MATH.h"
+
+
+/*HAL Includes*/
+#include "../../include/HAL/TFT/TFT_Interface.h"
+#include "../../include/HAL/TFT/TFT_cfg.h"
+#include "../../include/HAL/TFT/TFT_Private.h"
+
+/*MCAL Includes*/
+#include "../../include/MCAL/GPIO/GPIO_Interface.h"
+
+
+
